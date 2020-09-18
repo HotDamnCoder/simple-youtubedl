80fc3788204b2bbe2277e227562f5979d5833b45
HotDamnCoder <marcusb2kl@gmail.com>
2020-09-19 00:06:43 +0300

    Some Fixes

--- simple-youtubedl.py
+++ simple-youtubedl.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
 import youtube_dl
 
-
-OUTPUT_FOLDER = 'C:\\Users\\marcus\\Desktop\\Projects\\simple-youtubedl\\output\\'
-FFMPEG_LOCATION = 'C:\\Users\\marcus\\Desktop\\Projects\\simple-youtubedl\\'
-URL = 'https://www.youtube.com/playlist?list=PLjc__tvX8FXL7nzbY3eMxL_VbuCZWHAVU'
+FILE_DIRECTORY = '\\'.join(__file__.split('/')[:-1]) + '\\'
+OUTPUT_FOLDER = FILE_DIRECTORY + 'output\\'
+FFMPEG_LOCATION = FILE_DIRECTORY
+URL = 'https://www.youtube.com/playlist?list=PLjc__tvX8FXKothrBUuglRrVL514Xk28m'
 ydl_opts = {
     'format': 'bestaudio/best',
     'writethumbnail': True,
@@ -13,8 +13,9 @@ ydl_opts = {
         {'key': 'EmbedThumbnail'},
         {'key': 'FFmpegMetadata'}
     ],
+    'ignoreerrors': True,
     'ffmpeg_location': FFMPEG_LOCATION + 'ffmpeg.exe',
-    'outtmpl': OUTPUT_FOLDER + '%(title)s.%(ext)s'
+    'outtmpl': OUTPUT_FOLDER + '%(playlist)s/%(title)s.%(ext)s'
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([URL])
