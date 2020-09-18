b1ba4f4c7d78883b1976ae2c9b56e10057b2b799
HotDamnCoder <marcusb2kl@gmail.com>
2020-09-18 18:55:33 +0300

    Added constants

--- simple-youtubedl.py
+++ simple-youtubedl.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals
 import youtube_dl
 
+
+OUTPUT_FOLDER = 'C:\\Users\\marcus\\Desktop\\Projects\\simple-youtubedl\\output\\'
+FFMPEG_LOCATION = 'C:\\Users\\marcus\\Desktop\\Projects\\simple-youtubedl\\'
+URL = 'https://www.youtube.com/playlist?list=PLjc__tvX8FXL7nzbY3eMxL_VbuCZWHAVU'
 ydl_opts = {
     'format': 'bestaudio/best',
     'writethumbnail': True,
@@ -9,9 +13,9 @@ ydl_opts = {
         {'key': 'EmbedThumbnail'},
         {'key': 'FFmpegMetadata'}
     ],
-    'ffmpeg_location': 'C:\\Users\\marcus\\Desktop\\Projects\\simple-youtubedl\\ffmpeg.exe',
-    'outtmpl': 'C:\\Users\\marcus\\Desktop\\Projects\\simple-youtubedl\\output\\%(title)s.%(ext)s'
+    'ffmpeg_location': FFMPEG_LOCATION + 'ffmpeg.exe',
+    'outtmpl': OUTPUT_FOLDER + '%(title)s.%(ext)s'
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/playlist?list=PLjc__tvX8FXL7nzbY3eMxL_VbuCZWHAVU'])
+    ydl.download([URL])
 
