0696e97daea4275df0b1a7a69afc6c165b5d69a6
HotDamnCoder <marcusb2kl@gmail.com>
2021-04-04 18:36:07 +0300

    Updated

--- simple-youtubedl.py
+++ simple-youtubedl.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals
 import youtube_dl
-
+import sys
 FILE_DIRECTORY = '\\'.join(__file__.split('/')[:-1]) + '\\'
 OUTPUT_FOLDER = FILE_DIRECTORY + 'output\\'
 FFMPEG_LOCATION = FILE_DIRECTORY
-URL = 'https://www.youtube.com/playlist?list=PLjc__tvX8FXKothrBUuglRrVL514Xk28m'
+URL = sys.argv[1]
+print(URL)
 ydl_opts = {
     'format': 'bestaudio/best',
     'writethumbnail': True,
